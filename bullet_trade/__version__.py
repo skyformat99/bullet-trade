1ffd0a08b24ea5ab833d96e4d79fb1c3ab8dbfb6
BulletTrade <groundlee@gmail.com>
2026-01-18 21:48:47 +0800

    v0.6.1

--- bullet_trade/__version__.py
+++ bullet_trade/__version__.py
@@ -1,6 +1,6 @@
 """版本信息"""
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 __author__ = 'BulletTrade Team'
 __email__ = 'groundlee@gmail.com'
 __url__ = 'https://github.com/BulletTrade/bullet-trade'
